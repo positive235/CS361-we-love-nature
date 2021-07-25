bf965e65828cd371823c3cd263210ab7589c1d9a
Hae-Ji Park <positive235@gmail.com>
2021-07-25 10:44:44 -0700

    tooltips for buttons to show brief description and 'how to use' modall

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@
 import './App.css'
 import React, { useState, useMemo } from 'react'
 import TinderCard from 'react-tinder-card'
+import { Popup, Header, Icon, Modal, List } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 import { FcLike } from 'react-icons/fc'
 import { AiFillDislike } from 'react-icons/ai'
 import { GrRefresh } from 'react-icons/gr'
+import { BiArrowBack } from 'react-icons/bi'
 
 if (localStorage.getItem("photos")) {
   var db = JSON.parse(localStorage.getItem("photos"))
@@ -35,6 +38,27 @@ function App () {
   localStorage.setItem("photos", JSON.stringify(db))
   const [characters, setCharacters] = useState(db)
   const [lastDirection, setLastDirection] = useState()
+  const [open, setOpen] = useState(false)
+
+  const buttonDescription = [
+    {
+      name: 'Back',
+      description: 'If you want to see the former nature image, press this button!',
+    },
+    {
+      name: 'Like',
+      description: 'If you like this image, press this button! Then the image will be swiped to the left and ❤️ will be added on the bottom of the image',
+    },
+    {
+      name: 'Dislike',
+      description: 'If you do NOT like this image, press this button! Then the image will be swiped to the right',
+    },
+    {
+      name: 'Refresh',
+      description: 'New set of nature images will be shown! But...you should wait for less than a minute to see new nature images...!',      
+    }
+  ]
+  
 
   const childRefs = useMemo(() => Array(db.length).fill(0).map(i => React.createRef()), [])
 
@@ -43,7 +67,7 @@ function App () {
     setLastDirection(direction)
     if (direction === 'left') db[index].liked = true
     if (direction === 'right') db[index].liked = false
-    alreadyRemoved.push(nameToDelete)
+    //alreadyRemoved.push(nameToDelete)
   }
 
   const outOfFrame = (name) => {
@@ -68,13 +92,83 @@ function App () {
     window.location.reload()
   }
 
+  // const showOldImages = () => {
+    
+  //   const characterPlusOld = characters.concat(db.filter(data => alreadyRemoved[0] === data.name))
+  //   console.log(alreadyRemoved)
+  //   alreadyRemoved.pop()
+  //   console.log('characters:', characters)
+  //   console.log('characterPlusOld', characterPlusOld)
+  //   setCharacters(characterPlusOld)
+  // }
+
   return (
     <div className="App">
+      <Modal
+        closeIcon
+        open={open}
+        trigger={<button className='how-to-use'><Icon name='book' /> How To Use</button>}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+      ><Header icon='book' content='How To Use' />
+      <Modal.Content>
+      
+      <List>
+        <List.Item>
+          <List.Header>Swipe Left</List.Header>If you LIKE the image, swipe it to the left.
+        </List.Item>
+        <List.Item>
+          <List.Header>Swipe Right</List.Header>If you DISLIKE the image, swipe it to the right.
+        </List.Item>
+        <List.Item>
+          <List.Header><BiArrowBack /> BACK button</List.Header>Former image
+        </List.Item>
+        <List.Item>
+          <List.Header><FcLike /> LIKE button</List.Header>If you LIKE the image, press this button!
+        </List.Item>
+        <List.Item>
+          <List.Header><AiFillDislike /> DISLIKE button</List.Header>If you DISLIKE the image, press this button!
+        </List.Item>
+        <List.Item>
+          <List.Header><GrRefresh /> REFRESH button</List.Header>New set of nature images
+        </List.Item>
+      </List>
+        
+      </Modal.Content>
+      <Header icon='book' content='How To Use (in detail)' />
+      <Modal.Content>
+        <List>
+          <List.Item>
+            <List.Header>Swipe images</List.Header>If you like an image and swipe the image to the LEFT, <FcLike /> will be added to the image.
+            If you do NOT like an image and swipe the image to the RIGHT, <FcLike /> will be removed the image or not be added to the image.
+          </List.Item>
+          <List.Item>
+            <List.Header><BiArrowBack /> BACK button</List.Header>
+            If you want to see the former nature image, press this 'Back' button.
+          </List.Item>
+          <List.Item>
+            <List.Header><FcLike /> LIKE button</List.Header>
+            If you like an image, press this 'Like' button. Then the image will be swiped to the left and <FcLike /> will be added to the image.
+          </List.Item>
+          <List.Item>
+            <List.Header><AiFillDislike /> DISLIKE button</List.Header>
+            If you do NOT like an image, press this 'Dislike' button. 
+            Then the image will be swiped to the right and <FcLike /> will be removed from the image or not be added to the image
+          </List.Item>
+          <List.Item>
+            <List.Header><GrRefresh /> REFRESH button</List.Header>
+            New set of nature images will be shown! But...you might wait for less than a minute to see new nature images.
+          </List.Item>
+        </List>
+      </Modal.Content>
+      </Modal>
+
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap" rel="stylesheet" />
-
+      
       <h1>We <FcLike/> Nature</h1>
+
       <div className='cardContainer'>
         {characters.map((character, index) =>
           <TinderCard ref={childRefs[index]} className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
@@ -85,9 +179,36 @@ function App () {
         )}
       </div>
       <div className='buttons'>
-        <button onClick={() => swipe('left')}><FcLike /></button>
-        <button onClick={() => swipe('right')}><AiFillDislike /></button>
-        <button onClick={() => refresh()}><GrRefresh /></button>
+        
+        <Popup
+          key={buttonDescription[0].name}
+          header={buttonDescription[0].name}
+          trigger={<button onClick={() => showOldImages()}><BiArrowBack /></button>}
+          content={buttonDescription[0].description}
+          position='bottom left'
+        />
+        <Popup
+          key={buttonDescription[1].name}
+          header={buttonDescription[1].name}
+          trigger={<button onClick={() => swipe('left')}><FcLike /></button>}
+          content={buttonDescription[1].description}
+          position='bottom center'
+        />
+        <Popup
+          key={buttonDescription[2].name}
+          header={buttonDescription[2].name}
+          trigger={<button onClick={() => swipe('right')}><AiFillDislike /></button>}
+          content={buttonDescription[2].description}
+          position='bottom center'
+        />
+
+        <Popup
+          key={buttonDescription[3].name}
+          header={buttonDescription[3].name}
+          trigger={<button onClick={() => refresh()}><GrRefresh /></button>}
+          content={buttonDescription[3].description}
+          position='bottom right'
+        />
       </div>
       {lastDirection === 'left' ?<h2 key={lastDirection} className='infoText'>You  <FcLike /></h2> : (lastDirection === 'right' ? <h2 key={lastDirection} className='infoText'>You  <AiFillDislike /></h2>:<h2 className='infoText'>Swipe left =<FcLike />, right = <AiFillDislike /> , or press a button</h2>)}
     </div>
